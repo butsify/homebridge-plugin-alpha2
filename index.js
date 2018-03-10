a59c89026e1f47eee6ccdfaa952cd5e84d9505da
butsify <tputz@me.com>
2018-03-10 14:39:31 +0100

    new version

--- index.js
+++ index.js
@@ -8,26 +8,6 @@ module.exports = function (homebridge) {
   homebridge.registerAccessory("homebridge-alpha2", "MyAwesomeSwitch", mySwitch);
 };
 
-mySwitch.prototype = {
-  getServices: function () {
-    let informationService = new Service.AccessoryInformation();
-    informationService
-      .setCharacteristic(Characteristic.Manufacturer, "My switch manufacturer")
-      .setCharacteristic(Characteristic.Model, "My switch model")
-      .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
- 
-    let switchService = new Service.Switch("My switch");
-    switchService
-      .getCharacteristic(Characteristic.On)
-        .on('get', this.getSwitchOnCharacteristic.bind(this))
-        .on('set', this.setSwitchOnCharacteristic.bind(this));
- 
-    this.informationService = informationService;
-    this.switchService = switchService;
-    return [informationService, switchService];
-  }
-};
- 
 function mySwitch(log, config) {
   this.log = log;
   this.getUrl = url.parse(config['getUrl']);
@@ -35,7 +15,24 @@ function mySwitch(log, config) {
 }
  
 mySwitch.prototype = {
- 
+    getServices: function () {
+        let informationService = new Service.AccessoryInformation();
+        informationService
+          .setCharacteristic(Characteristic.Manufacturer, "My switch manufacturer")
+          .setCharacteristic(Characteristic.Model, "My switch model")
+          .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
+     
+        let switchService = new Service.Switch("My switch");
+        switchService
+          .getCharacteristic(Characteristic.On)
+            .on('get', this.getSwitchOnCharacteristic.bind(this))
+            .on('set', this.setSwitchOnCharacteristic.bind(this));
+     
+        this.informationService = informationService;
+        this.switchService = switchService;
+        return [informationService, switchService];
+    },
+
   getSwitchOnCharacteristic: function (next) {
     const me = this;
     request({
